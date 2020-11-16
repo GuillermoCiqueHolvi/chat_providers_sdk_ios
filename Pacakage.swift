2d9a80a5d15b3e97ad2c46713cf2505f1a8b0ee4
Guillermo Cique <guillermo@holvi.com>
2020-11-16 15:02:33 +0100

    Update Pacakage.swift

--- Pacakage.swift
+++ Pacakage.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ChatProvidersSDK",
-            url: "https://github.com/GuillermoCiqueHolvi/chat_providers_sdk_ios/archive/2.9.1.zip",
-            checksum: "78e16d244a1d02747fc879a2f9faf3cec2feea2d2bc230db1902cf8e3065b26e"
+            url: "https://github.com/GuillermoCiqueHolvi/chat_providers_sdk_ios/archive/2.9.0.zip",
+            checksum: "80a1179016f84b7a103deb1cfaea00a31df6a7db5124a90d7ca1a5ce3dd6e738"
         )
     ]
 )
