8c548f9beb51440825f9ccbaf89e90cb79aa0a48
Guillermo Cique <guillermo@holvi.com>
2020-11-17 12:06:52 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,12 @@ let package = Package(
     products: [
         .library(name: "ChatProvidersSDK", targets: ["ChatProvidersSDK"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/GuillermoCiqueHolvi/sdkconfigurations_sdk_ios", .upToNextMajor(from: "1.1.7")),
+        .package(url: "https://github.com/GuillermoCiqueHolvi/commonui_sdk_ios", .upToNextMajor(from: "6.0.1")),
+        .package(url: "https://github.com/GuillermoCiqueHolvi/messaging_sdk_ios", .upToNextMajor(from: "3.8.1")),
+        .package(url: "https://github.com/GuillermoCiqueHolvi/messagingapi_sdk_ios", .upToNextMajor(from: "3.8.1"))
+    ],
     targets: [
         .binaryTarget(name: "ChatProvidersSDK", path: "ChatProvidersSDK.xcframework")
     ]
